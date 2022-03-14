5a7c5ff44a3259ce01cab0ffd8ae41d433673c6d
jarjar721 <jarp820@gmail.com>
2022-03-14 18:54:45 -0400

    Full Calendar connected and Horarios done!

--- src/app/views/personal/horarios/horarios.component.ts
+++ src/app/views/personal/horarios/horarios.component.ts
@@ -1,37 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
-import {
-  startOfDay,
-  endOfDay,
-  subDays,
-  addDays,
-  endOfMonth,
-  isSameDay,
-  isSameMonth,
-  addHours,
-} from 'date-fns';
-
-import {
-  CalendarEvent,
-  CalendarEventAction,
-  CalendarEventTimesChangedEvent,
-  CalendarView,
-} from 'angular-calendar';
-
-const colors: any = {
-  red: {
-    primary: '#ad2121',
-    secondary: '#FAE3E3',
-  },
-  blue: {
-    primary: '#1e90ff',
-    secondary: '#D1E8FF',
-  },
-  yellow: {
-    primary: '#e3bc08',
-    secondary: '#FDF1BA',
-  },
-};
+import { CalendarOptions } from '@fullcalendar/angular'; // useful for typechecking
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import listPlugin from '@fullcalendar/list';
+import esLocale from '@fullcalendar/core/locales/es';
 
 @Component({
   selector: 'app-horarios',
@@ -40,53 +13,103 @@ const colors: any = {
 })
 export class HorariosComponent implements OnInit {
 
-  
+  calendarElementaryOptions: CalendarOptions = {
+    locale: esLocale,
+    plugins: [ timeGridPlugin ],
+    initialView: 'timeGridWeek',
+    headerToolbar: {
+      start: 'title',
+      center: '',
+      end: 'Editar'
+    },
+    customButtons: {
+      Editar: {
+        text: 'Editar',
+        click: function() {
+          alert('clicked Editar Elementary button!');
+        }
+      }
+    },
+    slotLabelFormat: {
+      hour: 'numeric',
+      minute: '2-digit',
+      hour12: true
+    },
+    allDaySlot: false,
+    weekends: false,
+    nowIndicator: true,
+    slotMinTime: '08:30:00',
+    slotMaxTime: '15:00:00',
+    expandRows: true,
+    slotLabelInterval: '00:30'
+  };
+
+  calendarMiddleOptions: CalendarOptions = {
+    locale: esLocale,
+    plugins: [ timeGridPlugin ],
+    initialView: 'timeGridWeek',
+    headerToolbar: {
+      start: 'title',
+      center: '',
+      end: 'Editar'
+    },
+    customButtons: {
+      Editar: {
+        text: 'Editar',
+        click: function() {
+          alert('clicked Editar Middle School button!');
+        }
+      }
+    },
+    slotLabelFormat: {
+      hour: 'numeric',
+      minute: '2-digit',
+      hour12: true
+    },
+    allDaySlot: false,
+    weekends: false,
+    nowIndicator: true,
+    slotMinTime: '08:00:00',
+    slotMaxTime: '14:30:00',
+    expandRows: true,
+    slotLabelInterval: '00:30'
+  };
+
+  calendarHighOptions: CalendarOptions = {
+    locale: esLocale,
+    plugins: [ timeGridPlugin ],
+    initialView: 'timeGridWeek',
+    headerToolbar: {
+      start: 'title',
+      center: '',
+      end: 'Editar'
+    },
+    customButtons: {
+      Editar: {
+        text: 'Editar',
+        click: function() {
+          alert('clicked Editar High School button!');
+        }
+      }
+    },
+    slotLabelFormat: {
+      hour: 'numeric',
+      minute: '2-digit',
+      hour12: true
+    },
+    allDaySlot: false,
+    weekends: false,
+    nowIndicator: true,
+    slotMinTime: '07:30:00',
+    slotMaxTime: '14:30:00',
+    expandRows: true,
+    slotLabelInterval: '00:30'
+  };
+
   constructor() { }
 
-  viewDate: Date = new Date();
-
-  events: CalendarEvent[] = [
-    {
-      start: subDays(startOfDay(new Date()), 1),
-      end: addDays(new Date(), 1),
-      title: 'A 3 day event',
-      color: colors.red,
-      //actions: this.actions,
-      allDay: true,
-      resizable: {
-        beforeStart: true,
-        afterEnd: true,
-      },
-      draggable: true,
-    },
-    {
-      start: startOfDay(new Date()),
-      title: 'An event with no end date',
-      color: colors.yellow,
-      //actions: this.actions,
-    },
-    {
-      start: subDays(endOfMonth(new Date()), 3),
-      end: addDays(endOfMonth(new Date()), 3),
-      title: 'A long event that spans 2 months',
-      color: colors.blue,
-      allDay: true,
-    },
-    {
-      start: addHours(startOfDay(new Date()), 2),
-      end: addHours(new Date(), 2),
-      title: 'A draggable and resizable event',
-      color: colors.yellow,
-      //actions: this.actions,
-      resizable: {
-        beforeStart: true,
-        afterEnd: true,
-      },
-      draggable: true,
-    },
-  ];
-
   ngOnInit(): void {
+
   }
 
 }
--- src/app/views/personal/personal.module.ts
+++ src/app/views/personal/personal.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { CalendarModule, DateAdapter } from 'angular-calendar';
-import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+//Full Calendar
+import { FullCalendarModule } from '@fullcalendar/angular'; // must go before plugins
+import dayGridPlugin from '@fullcalendar/daygrid'; // a plugin!
+import interactionPlugin from '@fullcalendar/interaction'; // a plugin!
 
 import {
   AvatarModule,
@@ -21,6 +23,10 @@ import { PersonalRoutingModule } from './personal-routing.module';
 import { UsuariosComponent } from './usuarios/usuarios.component';
 import { HorariosComponent } from './horarios/horarios.component';
 
+FullCalendarModule.registerPlugins([ // register FullCalendar plugins
+  dayGridPlugin,
+  interactionPlugin
+]);
 
 @NgModule({
   declarations: [
@@ -40,10 +46,7 @@ import { HorariosComponent } from './horarios/horarios.component';
     ProgressModule,
     TableModule,
     TabsModule,
-    CalendarModule.forRoot({
-      provide: DateAdapter,
-      useFactory: adapterFactory,
-    }),
+    FullCalendarModule // register FullCalendar with you app
   ]
 })
 export class PersonalModule { }
